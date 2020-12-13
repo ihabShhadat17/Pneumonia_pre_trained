725cb005a55c12d2f81c4206cd60be86728ce270
ihab-shhadat <ikshhadat17@gmail.com>
2020-12-14 01:07:52 +0200

    Pneumonia_pre_trained

--- main.py
+++ main.py
@@ -1,12 +1,62 @@
-import os
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
 
-from model.fine_tuning import TransferLearning, MODELS
+from model.fine_tuning import TransferLearning, plot_history, MODELS, plot_cm
 
-a = TransferLearning()
-model = a.get_fine_tuning_model(MODELS[0])
+transfer_learning = TransferLearning()
+for model in MODELS:
+    print(model)
+model_name = input('SELECT PRE_TRAINED MODEL:')
+base_model, input_shape = transfer_learning.get_fine_tuning_model(model_name)
+train_dataset, validation_dataset = transfer_learning.prepare_dataset()
+print(train_dataset.class_indices)
+print(sum(validation_dataset.labels) / validation_dataset.samples)
+print(sum(train_dataset.labels) / train_dataset.samples)
+
+base_model.trainable = False
+base_model.summary()
+inputs = tf.keras.Input(shape=input_shape)
+
+x = base_model(inputs, training=False)
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = tf.keras.layers.Dense(1)(x)
+model = tf.keras.Model(inputs, outputs)
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 model.summary()
-train, val = a.prepare_dataset()
-print(train.class_indices)
-print(sum(val.labels)/val.samples)
-print(sum(train.labels)/train.samples)
+len(model.trainable_variables)
+initial_epochs = 5
+
+loss0, accuracy0 = model.evaluate(validation_dataset)
+history = model.fit(train_dataset,
+                    epochs=initial_epochs,
+                    validation_data=validation_dataset)
+
+plot_history(history, model_name)
+base_model.trainable = False
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 5
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = True
+fine_tune_epochs = 5
+total_epochs = initial_epochs + fine_tune_epochs
+
+history_fine = model.fit(train_dataset,
+                         epochs=total_epochs,
+                         initial_epoch=history.epoch[-1],
+                         validation_data=validation_dataset)
+plot_history(history, model_name + '_FineTuning', initial_epochs=initial_epochs, is_fine_tuning=True)
+ypre = model.predict(validation_dataset)
+
+for name, value in zip(model.metrics_names, ypre):
+    print(name, ': ', value)
+
+plot_cm(model_name, validation_dataset.labels, ypre)
+model.save('models/' + model_name + '.h5')
--- model/fine_tuning.py
+++ model/fine_tuning.py
@@ -1,38 +1,141 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import os
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import numpy as np
 
-from tensorflow.keras.preprocessing import image_dataset_from_directory
+MODELS = {'VGG16': (224, 224),
+          'VGG19': (224, 224),
+          'InceptionV3': (299, 299),
+          'Xception': (299, 299),
+          'DenseNet201': (224, 224),
+          'MobileNetV2': (224, 224),
+          'InceptionResNetV2': (299, 299),
+          'ResNet50': (224, 224),
+          'ResNet50V2': (224, 224),
+          }
 
-MODELS = ['MobileNetV2']
+
+def plot_history(history, model, initial_epochs=None, is_fine_tuning=False):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    if not is_fine_tuning:
+        plt.figure(figsize=(8, 8))
+        plt.subplot(2, 1, 1)
+        plt.plot(acc, label='Training Accuracy')
+        plt.plot(val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.ylabel('Accuracy')
+        plt.ylim([min(plt.ylim()), 1])
+        plt.title('Training and Validation Accuracy ' + model)
+
+        plt.subplot(2, 1, 2)
+        plt.plot(loss, label='Training Loss')
+        plt.plot(val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.ylabel('Cross Entropy')
+        plt.ylim([0, 1.0])
+        plt.title('Training and Validation Loss')
+        plt.xlabel('epoch')
+        plt.savefig('figures/' + model + '.png')
+    else:
+        plt.figure(figsize=(8, 8))
+        plt.subplot(2, 1, 1)
+        plt.plot(acc, label='Training Accuracy')
+        plt.plot(val_acc, label='Validation Accuracy')
+        plt.ylim([0.8, 1])
+        plt.plot([initial_epochs - 1, initial_epochs - 1],
+                 plt.ylim(), label='Start Fine Tuning')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(2, 1, 2)
+        plt.plot(loss, label='Training Loss')
+        plt.plot(val_loss, label='Validation Loss')
+        plt.ylim([0, 1.0])
+        plt.plot([initial_epochs - 1, initial_epochs - 1],
+                 plt.ylim(), label='Start Fine Tuning')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.xlabel('epoch')
+        plt.savefig('figures/' + model + '.png')
+
+
+def plot_cm(model, labels, predictions, p=0.5):
+    cm = confusion_matrix(labels, predictions > p)
+    plt.figure(figsize=(5, 5))
+    sns.heatmap(cm, annot=True, fmt="d")
+    plt.title('Confusion matrix @{:.2f}'.format(p))
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+
+    print('Legitimate Transactions Detected (True Negatives): ', cm[0][0])
+    print('Legitimate Transactions Incorrectly Detected (False Positives): ', cm[0][1])
+    print('Fraudulent Transactions Missed (False Negatives): ', cm[1][0])
+    print('Fraudulent Transactions Detected (True Positives): ', cm[1][1])
+    print('Total Fraudulent Transactions: ', np.sum(cm[1]))
+    plt.savefig('figures/' + 'confusion_matrix_' + model + '.png')
 
 
 class TransferLearning:
     IMG_HEIGHT = None
     IMG_WIDTH = None
     IMG_SIZE = None
-    BATCH_SIZE = None
+    BATCH_SIZE = 32
     DATASET = 'dataset/chest_xray'
 
-    def __init__(self, IMG_HEIGHT=160, IMG_WIDTH=160):
-        self.IMG_HEIGHT = IMG_HEIGHT
-        self.IMG_WIDTH = IMG_WIDTH
-        self.IMG_SIZE = (IMG_HEIGHT, IMG_WIDTH, 3)
+    def __init__(self):
+        self.IMG_HEIGHT = None
+        self.IMG_WIDTH = None
+        self.IMG_SIZE = None
 
     def get_fine_tuning_model(self, model):
-        fine_tuning_models = {'MobileNetV2': tf.keras.applications.MobileNetV2(input_shape=self.IMG_SIZE,
+        shape = MODELS.get(model)
+        self.IMG_HEIGHT = shape[0]
+        self.IMG_WIDTH = shape[1]
+        self.IMG_SIZE = shape + (3,)
+        fine_tuning_models = {'VGG16': tf.keras.applications.VGG16(input_shape=self.IMG_SIZE,
+                                                                   include_top=False,
+                                                                   weights='imagenet'),
+                              'VGG19': tf.keras.applications.VGG19(input_shape=self.IMG_SIZE,
+                                                                   include_top=False,
+                                                                   weights='imagenet'),
+                              'InceptionV3': tf.keras.applications.InceptionV3(input_shape=self.IMG_SIZE,
                                                                                include_top=False,
                                                                                weights='imagenet'),
+                              'Xception': tf.keras.applications.Xception(input_shape=self.IMG_SIZE,
+                                                                         include_top=False,
+                                                                         weights='imagenet'),
+                              'DenseNet201': tf.keras.applications.DenseNet201(input_shape=self.IMG_SIZE,
+                                                                               include_top=False,
+                                                                               weights='imagenet'),
+                              'MobileNetV2': tf.keras.applications.MobileNetV2(input_shape=self.IMG_SIZE,
+                                                                               include_top=False,
+                                                                               weights='imagenet'),
+                              'InceptionResNetV2': tf.keras.applications.InceptionResNetV2(input_shape=self.IMG_SIZE,
+                                                                                           include_top=False,
+                                                                                           weights='imagenet'),
+                              'ResNet50': tf.keras.applications.ResNet50(input_shape=self.IMG_SIZE,
+                                                                         include_top=False,
+                                                                         weights='imagenet'),
+                              'ResNet50V2': tf.keras.applications.ResNet50V2(input_shape=self.IMG_SIZE,
+                                                                             include_top=False,
+                                                                             weights='imagenet'),
                               }
-        return fine_tuning_models.get(model)
+        return fine_tuning_models.get(model), self.IMG_SIZE
 
     def prepare_dataset(self):
         train_datagen = ImageDataGenerator(rescale=1. / 255,
-                                           shear_range=0.2,
-                                           zoom_range=0.2,
-                                           horizontal_flip=True,
+                                           rotation_range=24,
+                                           width_shift_range=0.15,
+                                           height_shift_range=0.2,
+                                           fill_mode='constant',
+                                           shear_range=16,
+
                                            validation_split=0.4)  # set validation split
 
         train_generator = train_datagen.flow_from_directory(
@@ -50,4 +153,5 @@ class TransferLearning:
             class_mode='binary',
             seed=123,
             subset='validation')  # set as validation data
+
         return train_generator, validation_generator
